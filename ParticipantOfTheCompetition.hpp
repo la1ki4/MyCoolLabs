ee947f62fcfb1e35a3cfd36c3b0f22e328dbfcc1
la1ki4 <100294709+la1ki4@users.noreply.github.com>
2023-04-05 14:29:17 +0300

    Update ParticipantOfTheCompetition.hpp

--- ParticipantOfTheCompetition.hpp
+++ ParticipantOfTheCompetition.hpp
@@ -1,95 +1,37 @@
+#ifndef _PARTICIPANTS_H_
+#define _PARTICIPANTS_H_
+
 #include <iostream>
-#include <sstream>
 #include <string>
+#include <vector>
 #include "Date.hpp"
 
-class ParticipantOfTheCompetition : public Date
+class ParticipantsOfTheCompetition : public Date
 {
-
 private:
-    //Инициализируем поля из варианта структуры
-    std::string lastName;
-    std::string firstName;
-    std::string country;
-    std::string category;
-    Date birthYear;
-    short idNumberInTable;
+	std::string last_name;
+	std::string first_name;
+	std::string _category;
+	std::string _country;
+	Date birth_year;
+	unsigned short id_Participant;
 
 public:
-    //Инициализируем конструктор по умолчанию
-    ParticipantOfTheCompetition()
-    {
-        this->lastName = "\0";
-        this->firstName = "\0";
-        this->category = "\0";
-        this->country = "\0";
-        idNumberInTable = 0;
-    }
+	ParticipantsOfTheCompetition() = default;
+	ParticipantsOfTheCompetition(const std::string&, const std::string&, const std::string&, const std::string&, const Date&, const unsigned short&);
+	ParticipantsOfTheCompetition(const ParticipantsOfTheCompetition&);
+	const ParticipantsOfTheCompetition& operator=(const ParticipantsOfTheCompetition&);
+	bool operator==(const ParticipantsOfTheCompetition&) const;
 
-
-    //Инициализируем конструктор копирования
-    ParticipantOfTheCompetition(const ParticipantOfTheCompetition& participant)
-    {
-        this->lastName = participant.lastName;
-        this->firstName = participant.firstName;
-        this->country = participant.country;
-        this->category = participant.category;
-        this->birthYear = participant.birthYear;
-        this->idNumberInTable = participant.idNumberInTable;
-    }
-
-    //Деструктор
-    virtual ~ParticipantOfTheCompetition()
-    {
-
-    }
-
-    //перегрузка оператора присваивания
-    ParticipantOfTheCompetition& operator=(const ParticipantOfTheCompetition& newPartcipant)
-    {
-        lastName = newPartcipant.lastName;
-        firstName = newPartcipant.firstName;
-        country = newPartcipant.country;
-        category = newPartcipant.category;
-        birthYear = newPartcipant.birthYear;
-        idNumberInTable = newPartcipant.idNumberInTable;
-
-
-        return *this;
-    }
-
-
-    //перегрузка оператора сравнения
-    bool operator==(const short& id)
-    {
-        return this->idNumberInTable == id;
-    }
-
-    //прототип перегрузки оператора записи в поток
-    friend std::ostream& operator<<(std::ostream& out, const ParticipantOfTheCompetition& Participant);
-
-    //прототип перегрузки оператора чтения из потока
-    friend std::istream& operator>>(std::istream& in, ParticipantOfTheCompetition& Participant);
+	std::string LastName() const;
+	std::string FirstName() const;
+	std::string Category() const;
+	std::string Country() const;
+	Date BirthYear() const;
+	unsigned short IDParticipant() const;
 };
 
-//перегрузка оператора записи в поток
-std::ostream& operator<<(std::ostream& out, const ParticipantOfTheCompetition& Participant)
-{
-    out << Participant.lastName << "\t" << Participant.firstName << "\t" << Participant.country << "\t" << Participant.category << "\t" << Participant.birthYear << "\t" << Participant.idNumberInTable;
+std::istream& operator>>(std::istream&, ParticipantsOfTheCompetition&);
+std::ostream& operator<<(std::ostream&, const ParticipantsOfTheCompetition&);
 
-    return out;
-}
-
-//перегрузка оператора чтения из потока
-std::istream& operator>>(std::istream& in, ParticipantOfTheCompetition& Participant)
-{
-    getline(in, Participant.lastName, ',');
-    getline(in, Participant.firstName, ',');
-    getline(in, Participant.country, ',');
-    getline(in, Participant.category, ',');
-    in >> Participant.birthYear;
-    in.ignore();
-    in >> Participant.idNumberInTable;
-
-    return in;
-}
+#endif
